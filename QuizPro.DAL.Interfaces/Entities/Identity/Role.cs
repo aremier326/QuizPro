a816e91dc48c23c80da7100d793b836e43cd4c75
Atem Johnson <Atem Johnson@ARTEMPC>
2023-03-24 10:26:38 +0200

    Add project files.

---
+++ QuizPro.DAL.Interfaces/Entities/Identity/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace QuizPro.DAL.Interfaces.Entities.Identity
+{
+    public class Role : IdentityRole<int>
+    {
+    }
+}
---
+++ QuizPro.DAL.Interfaces/Entities/Identity/RoleClaim.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace QuizPro.DAL.Interfaces.Entities.Identity
+{
+    internal class RoleClaim : IdentityRole<int>
+    {
+    }
+}
